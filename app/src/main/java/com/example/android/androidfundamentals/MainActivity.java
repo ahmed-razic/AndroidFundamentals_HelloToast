c01f882be5df760060ddb9c08aeb498043f0887d
ahmed-razic <Ibrahim2012*>
2021-02-03 14:55:52 +0100

    added lesson 1.1

--- app/src/main/java/com/example/android/androidfundamentals/MainActivity.java
+++ app/src/main/java/com/example/android/androidfundamentals/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.androidfundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "This is a I message");
+/*        Log.d("MainActivity", "This is a D message");
+        Log.w("MainActivity", "This is a E message");
+        Log.e("MainActivity", "This is a W message");*/
     }
 }
